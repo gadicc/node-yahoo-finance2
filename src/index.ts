ae5099310c6685cdce73780c76516f53d92e3740
Gadi Cohen <dragon@wastelands.net>
2021-02-01 15:24:55 +0200

    fix(index): default export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ import historical from './modules/historical';
 import quoteSummary from './modules/quoteSummary';
 import search from './modules/search';
 
+export default {
+  autoc,
+  historical,
+  quoteSummary,
+  search,
+}
+
 export {
   autoc,
   historical,
