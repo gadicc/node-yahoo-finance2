a4973b59647b218671677e9440e9cfec2f9fe06e
Gadi Cohen <dragon@wastelands.net>
2021-02-04 10:32:24 +0200

    chore(historical): add test for transformWith period2 today default

--- src/modules/historical.spec.ts
+++ src/modules/historical.spec.ts
@@ -21,6 +21,21 @@ describe('historical', () => {
     }, { devel: "historical-AAPL-2020-01-01-to-2020-01-03.json"});
   });
 
+  describe('transformWith', () => {
+
+    const yf = { _moduleExec: jest.fn(), historical };
+    yf.historical('symbol', { period1: 'required-but-not-used' });
+    const { transformWith } = yf._moduleExec.mock.calls[0][0].query;
+
+    it("uses today's date as default for period2", () => {
+      const now = new Date();
+      const options = transformWith({ period1: '2020-01-01' });
+      expect(options.period2).toBeDefined();
+      expect(options.period2).toBe(Math.floor(now.getTime() / 1000));
+    });
+
+  });
+
   it('throws InvalidOptions on invalid options', async () => {
     const rwo = (options:any) => yf.historical('symbol', options);
     await expect(rwo({ invalid: true })).rejects.toThrow(InvalidOptionsError)
