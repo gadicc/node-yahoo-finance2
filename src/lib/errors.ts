2c0f5221765ca657b1516532359b0c65a9afe012
Gadi Cohen <dragon@wastelands.net>
2021-02-05 07:50:27 +0200

    chore(errors): switch to typescript

---
+++ src/lib/errors.ts
@@ -0,0 +1,5 @@
+export class BadRequestError extends Error { name = 'BadRequestError' }
+export class HTTPError extends Error { name = 'HTTPError' }
+export class FailedYahooValidationError extends Error { name = 'FailedYahooValidationError' }
+export class InvalidOptionsError extends Error { name = 'InvalidOptionsError' }
+export class NoEnvironmentError extends Error { name = 'NoEnvironmentError' }
