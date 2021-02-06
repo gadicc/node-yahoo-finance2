0abe161b1d8784bac02019d9b7d69261e8929b7a
Gadi Cohen <dragon@wastelands.net>
2021-02-06 07:42:00 +0200

    chore(quoteSummary): add tests for 0P000071W8.TO (#20)

--- src/modules/quoteSummary.spec.ts
+++ src/modules/quoteSummary.spec.ts
@@ -13,10 +13,11 @@ const yf = {
 };
 
 const testSymbols = [
-  'AAPL',    // NMS (Nasdaq)
-  'OCDO.L',  // LSE
-  'BABA',    // NYSE
-  'QQQ',     // ETF
+  'AAPL',          // NMS (Nasdaq)
+  'OCDO.L',        // LSE
+  'BABA',          // NYSE
+  'QQQ',           // ETF
+  '0P000071W8.TO', // Mutual Fund
 ];
 
 interface itValidatesOpts {
