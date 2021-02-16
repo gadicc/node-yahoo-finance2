8543c835a08d9b5b88569403c0b8e229c0ae0a46
Gadi Cohen <dragon@wastelands.net>
2021-02-16 08:20:24 +0200

    chore(tests): move recommendation mass-validations to its own describe

--- src/modules/recommendationsBySymbol.spec.ts
+++ src/modules/recommendationsBySymbol.spec.ts
@@ -14,10 +14,15 @@ const yf = {
 };
 
 describe("recommendationsBySymbol", () => {
-  // make sure it passes validation for some symbols
-  it.each(testSymbols)("passes validation for symbol '%s'", async (symbol) => {
-    const devel = `recommendationsBySymbol-${symbol}.json`;
-    await yf.recommendationsBySymbol(symbol, {}, { devel });
+
+  describe('passes validation', () => {
+
+    // make sure it passes validation for some symbols
+    it.each(testSymbols)("for symbol '%s'", async (symbol) => {
+      const devel = `recommendationsBySymbol-${symbol}.json`;
+      await yf.recommendationsBySymbol(symbol, {}, { devel });
+    });
+
   });
 
   // make sure it passes validation for multiple symbols
