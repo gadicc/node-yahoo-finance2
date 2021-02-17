4c697492756782caa1af9466089991e5a34b6231
advaiyalad <advaiya.lad@gmail.com>
2021-02-17 09:53:35 -0600

    chore(tests): use it.each with testSymbols for several test suites

--- src/modules/search.spec.ts
+++ src/modules/search.spec.ts
@@ -17,10 +17,11 @@ describe("search", () => {
   // See also common module tests in moduleExec.spec.js
 
   // validate different searches
-  testSearches.forEach((search) => {
-    it(`passed validation for search: ${search}`, async () => {
-      const devel = `search-${search}.json`;
-      await yf.search(search, {}, { devel });
-    });
-  });
+  it.each(testSearches)(
+    "passed validation for search '%s'",
+    async (testSearch) => {
+      const devel = `search-${testSearch}.json`;
+      await yf.search(testSearch, {}, { devel });
+    }
+  );
 });
