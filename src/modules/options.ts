224048e0f59923c666002fc42b3b5992059439a1
advaiyalad <advaiya.lad@gmail.com>
2021-03-20 15:27:03 -0500

    feat(optionsModule): add options module

---
+++ src/modules/options.ts
@@ -0,0 +1,96 @@
+import type {
+  ModuleOptions,
+  ModuleOptionsWithValidateTrue,
+  ModuleOptionsWithValidateFalse,
+  ModuleThis,
+} from "../lib/moduleCommon";
+import { Quote } from "./quote";
+
+export interface OptionsResult {
+  underlyingSymbol: string;
+  expirationDates: number[];
+  strikes: number[];
+  hasMiniOptions: boolean;
+  quote: Quote;
+  options: Option[];
+}
+
+export interface Option {
+  expirationDate: number;
+  hasMiniOptions: boolean;
+  calls: CallOrPut[];
+  puts: CallOrPut[];
+}
+
+export interface CallOrPut {
+  contractSymbol: string;
+  strike: number;
+  currency: string;
+  lastPrice: number;
+  change: number;
+  percentChange: number;
+  volume?: number;
+  openInterest?: number;
+  bid?: number;
+  ask: number;
+  contractSize: "REGULAR";
+  expiration: number;
+  lastTradeDate: number;
+  impliedVolatility: number;
+  inTheMoney: boolean;
+}
+
+export interface OptionsOptions {
+  formatted?: boolean;
+  lang?: string;
+  region?: string;
+}
+
+const queryOptionsDefaults: OptionsOptions = {
+  formatted: false,
+  lang: "en-US",
+  region: "US",
+};
+
+export default function options(
+  this: ModuleThis,
+  symbol: string,
+  queryOptionsOverrides: OptionsOptions,
+  moduleOptions?: ModuleOptionsWithValidateTrue
+): Promise<OptionsResult>;
+
+export default function options(
+  this: ModuleThis,
+  symbol: string,
+  queryOptionsOverrides: OptionsOptions,
+  moduleOptions?: ModuleOptionsWithValidateFalse
+): Promise<any>;
+
+export default function options(
+  this: ModuleThis,
+  symbol: string,
+  queryOptionsOverrides: OptionsOptions,
+  moduleOptions?: ModuleOptions
+): Promise<any> {
+  return this._moduleExec({
+    moduleName: "historical",
+
+    query: {
+      url: "https://query1.finance.yahoo.com/v7/finance/options/" + symbol,
+      schemaKey: "#/definitions/OptionsOptions",
+      defaults: queryOptionsDefaults,
+      overrides: queryOptionsOverrides,
+    },
+
+    result: {
+      schemaKey: "#/definitions/OptionsResult",
+      transformWith(result: any) {
+        if (!result.optionChain)
+          throw new Error("Unexpected result: " + JSON.stringify(result));
+        return result.optionChain.result[0];
+      },
+    },
+
+    moduleOptions,
+  });
+}
