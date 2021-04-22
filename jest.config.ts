af07200eb6385bc89461e8e40879c5f017457422
Gadi Cohen <dragon@wastelands.net>
2021-04-22 18:40:01 +0300

    chore(tests): update jest.config.ts to ignore /dist/ not /api/

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ const config: Config.InitialOptions = {
   preset: "ts-jest",
   setupFilesAfterEnv: ["<rootDir>/tests/setupTests.js"],
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "/api/"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   /*
   reporters: [
     '<rootDir>/tests/reporter.js',
