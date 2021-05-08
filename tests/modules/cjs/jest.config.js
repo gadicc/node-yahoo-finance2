f1a10b65ca4ef5d7ffedd4e0545ea50b2dffc427
Gadi Cohen <dragon@wastelands.net>
2021-05-08 11:09:08 +0300

    sanity tests

---
+++ tests/modules/cjs/jest.config.js
@@ -0,0 +1,23 @@
+const config = {
+  preset: "ts-jest/presets/default-esm",
+  //setupFilesAfterEnv: ["<rootDir>/tests/setupTests.js"],
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/api/"],
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: false, // true in main config, turned off here [modules/cjs]
+    },
+  },
+  moduleNameMapper: {
+    //"(.*)\\.js$": "$1", // turned off here [modules/cjs]
+  },
+  /*
+  reporters: [
+    '<rootDir>/tests/reporter.js',
+    '<rootDir>/tests/summary-reporter.js',
+  ],
+  */
+};
+
+module.exports = config;
---
+++ tests/modules/esm/tests/tests.spec.js
@@ -0,0 +1,7 @@
+import yahooFinance from "../../../../dist/esm/src/index-node.js";
+
+describe("esm", () => {
+  it("sanity", () => {
+    expect(yahooFinance._env).toBeDefined();
+  });
+});
