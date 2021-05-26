7b41d8e0f68bd1b467e2e2060c35559f8a885be4
Gadi Cohen <dragon@wastelands.net>
2021-05-26 13:48:52 +0100

    chore(tests): common console silent class for clarity

---
+++ tests/console.js
@@ -0,0 +1,18 @@
+let _console;
+
+const fakeConsole = {
+  warn() {},
+  log() {},
+  error() {},
+};
+
+function consoleSilent() {
+  _console = console;
+  console = fakeConsole;
+}
+
+function consoleRestore() {
+  console = _console;
+}
+
+export { consoleSilent, consoleRestore };
