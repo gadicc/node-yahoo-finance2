43e5c5d6ea48243d3844460c1d96b5837fa67fe6
Gadi Cohen <dragon@wastelands.net>
2021-12-23 00:33:34 +0000

    fix(pkg): warn to upgrade to @latest 2.x.

--- src/index-common.ts
+++ src/index-common.ts
@@ -18,6 +18,15 @@ import trendingSymbols from "./modules/trendingSymbols.js";
 // other
 import quoteCombine from "./other/quoteCombine.js";
 
+console.warn(
+  "[yahoo-finance2] Please upgrade to yahoo-finance2@latest, i.e. " +
+    "2.x.  The only breaking change is a fix in quoteSummary to " +
+    "correctly return Date objects instead of numerical timestamps in " +
+    "these submodules: defaultKeyStatistics, earningsHistory, insiderHolders. " +
+    "For more info see: " +
+    "https://github.com/gadicc/node-yahoo-finance2/commit/4cf1f624d55d92f80db4b36b3afe9414f5eb5a3c"
+);
+
 export default {
   // internal
   _env: {},
