463b124a985bf4c8d78f8be77b1b883f51a99501
Gadi Cohen <dragon@wastelands.net>
2022-02-16 17:28:38 +0200

    chore(tests): cache result of fetchDevel await import

--- src/env-node.ts
+++ src/env-node.ts
@@ -2,9 +2,13 @@ import { URLSearchParams } from "url";
 import fetch from "node-fetch";
 
 // This let's us still only require the file if we need it, at runtime.
+let fetchDevelFunc: Function;
 async function fetchDevel() {
+  if (fetchDevelFunc) return fetchDevelFunc;
+
   const module = await import("./lib/fetchDevel.js");
-  return module.default;
+  fetchDevelFunc = module.default;
+  return fetchDevelFunc;
 }
 
 export default {
