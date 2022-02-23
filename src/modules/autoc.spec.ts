99846ed5a6709c94a9cd1a8a4abd9c1d8c58f78f
Gadi Cohen <dragon@wastelands.net>
2022-02-23 16:03:30 +0200

    fix(autoc): throw error on use (fixes #337)

--- src/modules/autoc.spec.ts
+++ src/modules/autoc.spec.ts
@@ -1,21 +1,5 @@
 import autoc from "./autoc.js";
-import { testSymbols } from "../../tests/symbols.js";
-
-import testYf from "../../tests/testYf.js";
-
-const yf = testYf({ autoc });
 
 describe("autoc", () => {
-  // See also common module tests in moduleExec.spec.js
-
-  it.each(testSymbols)("passes validation for symbol '%s'", async (symbol) => {
-    await yf.autoc(symbol, {}, { devel: `autoc-${symbol}.json` });
-  });
-
-  if (process.env.FETCH_DEVEL !== "nocache")
-    it("throws on unexpected input", async () => {
-      await expect(
-        yf.autoc("AAPL", {}, { devel: "weirdJsonResult.fake.json" })
-      ).rejects.toThrow(/Unexpected result/);
-    });
+  it("throws", () => expect(autoc()).rejects.toThrow(/issues\/337/));
 });
