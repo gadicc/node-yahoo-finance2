d24ceeb7c885e870b24d3f12b26bbc6e34a0af5d
Kody Liou <kody.liou@gmail.com>
2022-10-29 20:43:35 +0800

    Switch to standard-with-typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,61 +1,17 @@
-const jsConfigs = {
-  root: true,
+module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
-    node: true,
-    'jest/globals': true,
+    node: true
   },
-  extends: ['airbnb-base'],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['prettier', 'import', 'jest'],
-  rules: {
-    'linebreak-style': 0,
-    'import/no-unresolved': 'error',
-    'prettier/prettier': [
-      'error',
-      {
-        tabWidth: 2,
-        endOfLine: 'auto',
-        singleQuote: true,
-        trailingComma: 'all',
-        printWidth: 80,
-        bracketSpacing: true,
-      },
-      {
-        usePrettierrc: false,
-      },
-    ],
-  },
-};
-
-const tsOverride = {
-  files: ['*.ts', '*.*.ts'],
-  extends: [
-    'airbnb-base',
-    'plugin:prettier/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier', 'import', 'jest'],
-  rules: {
-    ...jsConfigs.rules,
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
-  },
-};
-
-module.exports = {
-  ...jsConfigs,
+  extends: 'standard-with-typescript',
   overrides: [
-    tsOverride,
   ],
-};
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: {
+  }
+}
