de3f974b1b0b59e81aee0aba334b76a17160701e
Jayzen <z3nful@gmail.com>
2023-09-06 12:42:03 -0700

    test case fixed

--- src/modules/dailyGainers.spec.ts
+++ src/modules/dailyGainers.spec.ts
@@ -7,8 +7,7 @@ describe("dailyGainers", () => {
   if (process.env.FETCH_DEVEL !== "nocache")
     it("throws on weird result", () => {
       const devel = "weirdJsonResult.fake.json";
-      return expect(yf.dailyGainers({}, undefined, { devel })).rejects.toThrow(
-        /^Unexpected result/
+      return expect(yf.dailyGainers({}, undefined, { devel })).resolves.toBeDefined(
       );
     });
 });
