f41cfce2842ab0710d8758127fb39d1829e91ad5
Jayzen <z3nful@gmail.com>
2023-09-06 12:24:34 -0700

    feat(module): add dailyGainers

---
+++ src/modules/dailyGainers.spec.ts
@@ -0,0 +1,14 @@
+import dailyGainers from "./dailyGainers.js";
+import testYf from "../../tests/testYf.js";
+
+const yf = testYf({ dailyGainers });
+
+describe("dailyGainers", () => {
+  if (process.env.FETCH_DEVEL !== "nocache")
+    it("throws on weird result", () => {
+      const devel = "weirdJsonResult.fake.json";
+      return expect(yf.dailyGainers({}, undefined, { devel })).rejects.toThrow(
+        /^Unexpected result/
+      );
+    });
+});
