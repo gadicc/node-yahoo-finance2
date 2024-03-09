3c8558342bd92130c8a65247ab2f4b7947417710
shadaeiou <shadaeiou@gmail.com>
2024-03-08 21:59:28 -0500

    Forgot to run prettier... woops

--- src/modules/fundamentalsTimeSeries.spec.ts
+++ src/modules/fundamentalsTimeSeries.spec.ts
@@ -15,29 +15,44 @@ describe("fundamentalsTimeSeries", () => {
         period1: "2020-01-01",
         period2: "2021-01-01",
       },
-      { devel: `fundamentalsTimeSeries-${symbol}-2020-01-01-to-2021-01-01.json` }
+      {
+        devel: `fundamentalsTimeSeries-${symbol}-2020-01-01-to-2021-01-01.json`,
+      }
     );
   });
 
   it("throws if period1,period2 are the same", async () => {
     await expect(
-      yf.fundamentalsTimeSeries("TSLA", { period1: "2020-01-01", period2: "2020-01-01" })
+      yf.fundamentalsTimeSeries("TSLA", {
+        period1: "2020-01-01",
+        period2: "2020-01-01",
+      })
     ).rejects.toThrow(/cannot share the same value/);
   });
 
   it("throws if period{1,2} gets an invalid string for new Date()", async () => {
-    await expect(yf.fundamentalsTimeSeries("TSLA", { period1: "invalid", period2: "2021-01-01" })).rejects.toThrow(
-      /invalid date provided/
-    );
+    await expect(
+      yf.fundamentalsTimeSeries("TSLA", {
+        period1: "invalid",
+        period2: "2021-01-01",
+      })
+    ).rejects.toThrow(/invalid date provided/);
 
     await expect(
-      yf.fundamentalsTimeSeries("TSLA", { period1: "2020-01-011", period2: "invalid" })
+      yf.fundamentalsTimeSeries("TSLA", {
+        period1: "2020-01-011",
+        period2: "invalid",
+      })
     ).rejects.toThrow(/invalid date provided/);
   });
 
   it("throws if invalid type", async () => {
-    await expect(yf.fundamentalsTimeSeries("TSLA", { period1: "2020-01-01", period2: "2021-01-01", type: 'invalid' })).rejects.toThrow(
-      /option type invalid/
-    );
+    await expect(
+      yf.fundamentalsTimeSeries("TSLA", {
+        period1: "2020-01-01",
+        period2: "2021-01-01",
+        type: "invalid",
+      })
+    ).rejects.toThrow(/option type invalid/);
   });
 });
