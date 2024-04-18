3a8c459f54e74512a69074652c05a37e3917c5de
nocodehummel <nocodehummel@gmail.com>
2024-04-18 19:29:26 +0200

    PoC with jest-fetch-mock-cache.

--- src/lib/getCookies.spec.ts
+++ src/lib/getCookies.spec.ts
@@ -1,15 +1,23 @@
+import crypto from "crypto";
 import { expect, jest } from "@jest/globals";
+import fetchMock from "jest-fetch-mock";
+import { createCachingMock, NodeFSStore } from "jest-fetch-mock-cache";
 import { ExtendedCookieJar } from "./cookieJar";
 import { getCookies } from "./getCookies";
 import options from "./options";
+fetchMock.enableMocks();
 
-jest.mock("./cookieJar");
+//jest.mock("./cookieJar");
+
+const BASE_URL = "https://finance.yahoo.com";
+const cachingMock = createCachingMock({ store: new NodeFSStore() });
 
 describe("getCookies", () => {
   const { logger } = options;
   let cookieJar: ExtendedCookieJar;
 
   beforeEach(() => {
+    fetchMock.mockImplementation(cachingMock);
     cookieJar = new ExtendedCookieJar();
   });
 
@@ -18,8 +26,15 @@ describe("getCookies", () => {
   });
 
   it("cookieJar with random cookie string", async () => {
-    const cookies = await getCookies("myurl", cookieJar, {}, logger);
+    jest.spyOn(cookieJar, "getCookies").mockImplementation(() => {
+      return [crypto.randomBytes(32).toString("hex")];
+    });
+    const cookies = await getCookies(cookieJar, {}, logger, BASE_URL);
     expect(cookieJar.getCookies).toHaveBeenCalledTimes(1);
     expect(cookies).toBeTruthy();
+  });
+
+  it("cookieJar without cookies", async () => {
+    const cookies = await getCookies(cookieJar, {}, logger, BASE_URL);
   });
 });
--- src/lib/getCookies.ts
+++ src/lib/getCookies.ts
@@ -3,14 +3,56 @@ import { Logger } from "./options.js";
 
 // Prepare cookies for the API connection.
 export async function getCookies(
-  url: string,
   cookieJar: ExtendedCookieJar,
   fetchOptionsBase: RequestInit,
-  logger: Logger
+  logger: Logger,
+  url: string
 ) {
-  let cookies = await cookieJar.getCookies(url, {
+  const cookies = await cookieJar.getCookies(url, {
     expire: true,
   });
   if (cookies.length > 0) return true;
-  return false;
+
+  // This request will get our first cookies, so nothing to send.
+  logger.debug("Fetching cookies from " + url + "...");
+  const fetchOptions: RequestInit = {
+    ...fetchOptionsBase,
+    headers: {
+      ...fetchOptionsBase.headers,
+      // NB, we won't get a set-cookie header back without this:
+      accept: "text/html,application/xhtml+xml,application/xml",
+    },
+    redirect: "manual",
+  };
+
+  const response = await fetch(url, fetchOptions);
+  await cookieJar.setFromHeaders(response.headers, url);
+
+  // Redirects when consent is missing.
+  const location = response.headers.get("location");
+  if (location) {
+    if (location.match(/guce.yahoo/)) {
+      const consentFetchOptions: RequestInit = {
+        ...fetchOptions,
+        headers: {
+          ...fetchOptions.headers,
+          // GUCS=XXXXXXXX; Max-Age=1800; Domain=.yahoo.com; Path=/; Secure
+          cookie: await cookieJar.getCookieString(location),
+        },
+      };
+
+      // Returns 302 to collectConsent?sessionId=XXX
+      logger.debug("Fetch", location);
+      const consentResponse = await fetch(location, consentFetchOptions);
+      const consentLocation = consentResponse.headers.get("location");
+    } else {
+      throw new Error(
+        "Unsupported redirect to " + location + ", please report."
+      );
+    }
+  } else {
+    logger.debug("Consent is given.");
+  }
+
+  return true;
 }
