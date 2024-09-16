b6d4d2e8411713033650430e9a00260bf73d4e0a
Gadi Cohen <dragon@wastelands.net>
2024-09-16 13:36:09 +0100

    fix(chart): fix and improve non-Date handling code for periods

--- src/modules/chart.ts
+++ src/modules/chart.ts
@@ -218,12 +218,22 @@ export default async function chart(
         const dates = ["period1", "period2"] as const;
         for (const fieldName of dates) {
           const value = queryOptions[fieldName];
-          if (value instanceof Date)
+          if (value instanceof Date) {
             queryOptions[fieldName] = Math.floor(value.getTime() / 1000);
-          else typeof value === "string";
-          queryOptions[fieldName] = Math.floor(
-            new Date(value as string).getTime() / 1000,
-          );
+          } else if (typeof value === "string") {
+            const timestamp = new Date(value as string).getTime();
+
+            if (isNaN(timestamp))
+              throw new Error(
+                "yahooFinance.chart() option '" +
+                  fieldName +
+                  "' invalid date provided: '" +
+                  value +
+                  "'",
+              );
+
+            queryOptions[fieldName] = Math.floor(timestamp / 1000);
+          }
         }
 
         if (queryOptions.period1 === queryOptions.period2) {
