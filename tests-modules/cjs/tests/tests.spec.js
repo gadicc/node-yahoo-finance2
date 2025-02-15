8bd789112e12938acb5e535b16d66a194371761e
Gadi Cohen <dragon@wastelands.net>
2025-02-15 14:51:11 +0000

    chore(deno): initial move to deno, remove old tooling

--- tests-modules/cjs/tests/tests.spec.js
+++
@@ -1,7 +0,0 @@
-const yahooFinance = require("../../../dist/cjs/src/index-test.js").default;
-
-describe("esm", () => {
-  it("sanity", () => {
-    expect(yahooFinance._env).toBeDefined();
-  });
-});
--- tests-modules/esm/tests/tests.spec.js
+++
@@ -1,7 +0,0 @@
-import yahooFinance from "../../../dist/esm/src/index-test.js";
-
-describe("esm", () => {
-  it("sanity", () => {
-    expect(yahooFinance._env).toBeDefined();
-  });
-});
